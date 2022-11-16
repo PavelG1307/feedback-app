326f85f99a045500e48a32d4eee292ca08f43f8d
Pavel_prod <julu13@yandex.ru>
2022-11-16 23:42:49 +0300

    add: swagger, input dto, delete spec

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,21 +1,39 @@
 import { Controller, Get, Post, Put, Query } from '@nestjs/common'
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { GetReviewsDto } from './dto/get-reviews.dto'
-import { Reviews } from './models/review'
+import { ResponseGetReviewsDto } from './dto/res-get-update.dto'
+import { ResponseUpdateReviewsDto } from './dto/res-update-review.dto'
 import { ReviewsService } from './reviews.service'
 
+@ApiTags('Reviews')
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly ReviewsService: ReviewsService) {}
 
+  @ApiOperation({summary: 'Get reviews'})
+  @ApiResponse({ status: 200, type: ResponseGetReviewsDto })
+  @ApiBadRequestResponse({ description: 'Bad request' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
   @Get()
-  get(@Query() filters: GetReviewsDto){
-    const data = this.ReviewsService.get(filters)
-    return { success: !!data, data: data ?? null }
+  async get(@Query() filters: GetReviewsDto): Promise<ResponseGetReviewsDto> {
+    const data = await this.ReviewsService.get(filters)
+    return data
   }
 
+  @ApiOperation({summary: 'Start parsing reviews'})
+  @ApiResponse({ status: 200, type: ResponseUpdateReviewsDto })
+  @ApiBadRequestResponse({ description: 'Bad request' })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
   @Put()
-  update() {
-    const total = this.ReviewsService.update()
+  async update(): Promise<ResponseUpdateReviewsDto> {
+    const total = await this.ReviewsService.update()
     return { success: !!total, total: total ?? null }
   }
 }
