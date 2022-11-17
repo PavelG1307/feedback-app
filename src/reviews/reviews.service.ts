0ce06b46da564599ed5b60a91025f81a7bcbe45b
Pavel_prod <julu13@yandex.ru>
2022-11-17 15:33:24 +0300

    fix: refactoring filters

--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -15,28 +15,28 @@ export class ReviewsService {
   ) { }
 
   async get(filters: GetReviewsDto): Promise<ResponseGetReviewsDto> {
-    const { order, orderBy, limit, offset, ratedFrom, ratedTo, ...where} = filters
+    const { order, orderBy, limit, offset, ratedFrom, ratedTo, ...where } = filters
     const reviews = await this.reviewsModel.findAndCountAll({
-      order: [[filters.orderBy || 'rated', filters?.order || 'DESC']],
-      where: { ...where, isDeleted: false},
-      offset: filters.offset || 0,
-      limit: filters.limit || 20
+      order: [[orderBy || 'rated', order || 'DESC']],
+      where: { ...where, isDeleted: false },
+      offset: offset || 0,
+      limit: limit || 20
     })
-    if (!reviews?.rows || !reviews.rows.length) throw new HttpException('Not found', HttpStatus.NOT_FOUND)
+    if (!(reviews?.rows && reviews.rows.length)) throw new HttpException('Not found', HttpStatus.NOT_FOUND)
     return { count: reviews.count, reviews: reviews.rows }
   }
 
   async update(updateConfig: UpdateReviewsDto): Promise<number> | never {
     const { chainId, limit } = updateConfig
     const url = 'https://api.delivery-club.ru/api1.2/reviews'
-    const parseReviews = async (limit, offset, chainId): Promise<{total: number, reviews: Reviews[]}> | never => {
+    const parseReviews = async (limit, offset, chainId): Promise<{ total: number, reviews: Reviews[] }> | never => {
       const params = { chainId, limit, offset }
       const method = 'get'
       try {
-      const response = await this.httpService.get(url, { method, url, params }).toPromise()
-      const { total, reviews } = response.data
-      if (!(total && reviews)) throw new HttpException('Internal error', HttpStatus.INTERNAL_SERVER_ERROR)
-      return { total, reviews }
+        const response = await this.httpService.get(url, { method, url, params }).toPromise()
+        const { total, reviews } = response.data
+        if (!(total && reviews)) throw new HttpException('Internal error', HttpStatus.INTERNAL_SERVER_ERROR)
+        return { total, reviews }
       } catch (e) {
         console.log(e)
         throw new HttpException('Internal error', HttpStatus.INTERNAL_SERVER_ERROR)
@@ -45,7 +45,7 @@ export class ReviewsService {
 
     const { total, reviews } = await parseReviews(limit, 0, chainId)
     reviews.map(review => review.isDeleted = false)
-    await this.reviewsModel.update({ isDeleted: true }, { where: { isDeleted: false }})
+    await this.reviewsModel.update({ isDeleted: true }, { where: { isDeleted: false } })
     await this.reviewsModel.bulkCreate(reviews, { updateOnDuplicate: ['body', 'icon', 'answers'] })
 
     for (let i = 0; i < total / limit; i++) {
